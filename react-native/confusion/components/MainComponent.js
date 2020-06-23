dd355f09fe74ff923d2f916e40c410a75043d5e0
bansalsurya <2647surya@gmail.com>
2020-06-23 22:47:13 +0530

    added drawer feature swipe gesture

--- react-native/confusion/components/MainComponent.js
+++ react-native/confusion/components/MainComponent.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import Menu from './MenuComponent';
-import Dishdetail from './DishdetailComponent';
-import { DISHES } from '../shared/dishes';
 import Constants from 'expo-constants';
-
-import { View, Platform } from 'react-native';
+import { Platform } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
+import Menu from './MenuComponent';
+import Dishdetail from './DishdetailComponent';
+import Home from './HomeComponent';
 
 const MenuNavigator = createStackNavigator();
 
@@ -29,8 +30,47 @@ function RootMenuNavigator() {
     </MenuNavigator.Navigator>
   );
 }
+const HomeNavigator = createStackNavigator();
 
-export class MainComponent extends Component {
+function RootHomeNavigator() {
+  return (
+    <HomeNavigator.Navigator
+      initialRouteName='Home'
+      screenOptions={({ navigation }) => ({
+        headerStyle: {
+          backgroundColor: '#512DA8',
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+          color: '#fff',
+        },
+      })}
+    >
+      <HomeNavigator.Screen name='Home' component={Home} />
+    </HomeNavigator.Navigator>
+  );
+}
+
+const MainNavigator = createDrawerNavigator();
+
+function RootMainNavigator() {
+  return (
+    <MainNavigator.Navigator>
+      <MainNavigator.Screen
+        name='Home'
+        component={RootHomeNavigator}
+        options={{ title: 'Home', drawerLabel: 'Home' }}
+      />
+      <MainNavigator.Screen
+        name='Menu'
+        component={RootMenuNavigator}
+        options={{ title: 'Menu', drawerLabel: 'Menu' }}
+      />
+    </MainNavigator.Navigator>
+  );
+}
+
+class MainComponent extends Component {
   render() {
     return (
       <NavigationContainer
@@ -39,7 +79,7 @@ export class MainComponent extends Component {
           paddingTop: Platform.OS === 'ios' ? 0 : Constants.statusBarHeight,
         }}
       >
-        <RootMenuNavigator />
+        <RootMainNavigator />
       </NavigationContainer>
     );
   }
