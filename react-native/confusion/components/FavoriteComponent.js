66dcfccd5bdb4a96f68f0802ffe76b7bac420f4a
bansalsurya <2647surya@gmail.com>
2020-06-28 01:30:57 +0530

    Favorites:

--- react-native/confusion/components/FavoriteComponent.js
+++ react-native/confusion/components/FavoriteComponent.js
@@ -2,8 +2,11 @@ import React, { Component } from 'react';
 import { FlatList, View, Text } from 'react-native';
 import { ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
+import Swipeout from 'react-native-swipeout';
+
 import { baseUrl } from '../shared/baseUrl';
 import { Loading } from './LoadingComponent';
+import { deleteFavorite } from '../redux/ActionCreators';
 
 const mapStateToProps = (state) => {
   return {
@@ -11,6 +14,10 @@ const mapStateToProps = (state) => {
     favorites: state.favorites,
   };
 };
+
+const mapDispatchToProps = (dispatch) => ({
+  deleteFavorite: (dishId) => dispatch(deleteFavorite(dishId)),
+});
 
 export class Favorites extends Component {
   static screenOptions = {
@@ -20,15 +27,24 @@ export class Favorites extends Component {
     const { navigate } = this.props.navigation;
 
     const renderMenuItem = ({ item, index }) => {
+      const rightButton = [
+        {
+          text: 'Delete',
+          type: 'delete',
+          onPress: () => this.props.deleteFavorite(item.id),
+        },
+      ];
       return (
-        <ListItem
-          key={index}
-          title={item.name}
-          subtitle={item.description}
-          hideChevron={true}
-          onPress={() => navigate('Dishdetail', { dishId: item.id })}
-          leftAvatar={{ source: { uri: baseUrl + item.image } }}
-        />
+        <Swipeout right={rightButton} autoClose={true}>
+          <ListItem
+            key={index}
+            title={item.name}
+            subtitle={item.description}
+            hideChevron={true}
+            onPress={() => navigate('Dishdetail', { dishId: item.id })}
+            leftAvatar={{ source: { uri: baseUrl + item.image } }}
+          />
+        </Swipeout>
       );
     };
     if (this.props.dishes.dishes.isLoading) {
@@ -53,4 +69,4 @@ export class Favorites extends Component {
   }
 }
 
-export default connect(mapStateToProps)(Favorites);
+export default connect(mapStateToProps, mapDispatchToProps)(Favorites);
