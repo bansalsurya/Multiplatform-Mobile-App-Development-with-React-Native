7355e477c42e08c8e0e7ff5a276b3cdf252a888a
bansalsurya <2647surya@gmail.com>
2020-06-28 19:23:04 +0530

    animations

--- react-native/confusion/components/ContactComponent.js
+++ react-native/confusion/components/ContactComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 import { Card } from 'react-native-elements';
+import * as Animatable from 'react-native-animatable';
 
 export class ContactComponent extends Component {
   static options = {
@@ -8,15 +9,17 @@ export class ContactComponent extends Component {
   };
   render() {
     return (
-      <Card title='Contact Information'>
-        <View style={{ margin: 10 }}>
-          <Text style={{ margin: 10 }}>Clear Water Bay, Kowloon</Text>
-          <Text style={{ margin: 10 }}>HONG KONG</Text>
-          <Text style={{ margin: 10 }}>Tel: +852 1234 5678</Text>
-          <Text style={{ margin: 10 }}>Fax: +852 8765 4321</Text>
-          <Text style={{ margin: 10 }}>Email:confusion@food.net</Text>
-        </View>
-      </Card>
+      <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
+        <Card title='Contact Information'>
+          <View style={{ margin: 10 }}>
+            <Text style={{ margin: 10 }}>Clear Water Bay, Kowloon</Text>
+            <Text style={{ margin: 10 }}>HONG KONG</Text>
+            <Text style={{ margin: 10 }}>Tel: +852 1234 5678</Text>
+            <Text style={{ margin: 10 }}>Fax: +852 8765 4321</Text>
+            <Text style={{ margin: 10 }}>Email:confusion@food.net</Text>
+          </View>
+        </Card>
+      </Animatable.View>
     );
   }
 }
--- react-native/confusion/components/DishdetailComponent.js
+++ react-native/confusion/components/DishdetailComponent.js
@@ -12,6 +12,8 @@ import { Card, Icon, Rating, Input } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
 import { postFavorite, postComment } from '../redux/ActionCreators';
+import * as Animatable from 'react-native-animatable';
+
 const mapStateToProps = (state) => {
   return {
     dishes: state.dishes,
@@ -31,29 +33,31 @@ function RenderDish(props) {
 
   if (dish != null) {
     return (
-      <Card featuredTitle={dish.name} image={{ uri: baseUrl + dish.image }}>
-        <Text style={{ margin: 10 }}>{dish.description}</Text>
-        <Icon
-          raised
-          reverse
-          name={props.favorite ? 'heart' : 'heart-o'}
-          type='font-awesome'
-          color='#f50'
-          onPress={() =>
-            props.favorite ? console.log('Already favorite') : props.onPress()
-          }
-        />
-        <Icon
-          raised
-          reverse
-          name={'pencil'}
-          type='font-awesome'
-          color='#512DA8'
-          onPress={() => {
-            props.toggleModal();
-          }}
-        />
-      </Card>
+      <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
+        <Card featuredTitle={dish.name} image={{ uri: baseUrl + dish.image }}>
+          <Text style={{ margin: 10 }}>{dish.description}</Text>
+          <Icon
+            raised
+            reverse
+            name={props.favorite ? 'heart' : 'heart-o'}
+            type='font-awesome'
+            color='#f50'
+            onPress={() =>
+              props.favorite ? console.log('Already favorite') : props.onPress()
+            }
+          />
+          <Icon
+            raised
+            reverse
+            name={'pencil'}
+            type='font-awesome'
+            color='#512DA8'
+            onPress={() => {
+              props.toggleModal();
+            }}
+          />
+        </Card>
+      </Animatable.View>
     );
   } else {
     return <View></View>;
@@ -75,13 +79,15 @@ function RenderComments(props) {
   };
 
   return (
-    <Card title='Comments'>
-      <FlatList
-        data={comments}
-        renderItem={renderCommentItem}
-        keyExtractor={(item) => item.id.toString()}
-      />
-    </Card>
+    <Animatable.View animation='fadeInUp' duration={2000} delay={1000}>
+      <Card title='Comments'>
+        <FlatList
+          data={comments}
+          renderItem={renderCommentItem}
+          keyExtractor={(item) => item.id.toString()}
+        />
+      </Card>
+    </Animatable.View>
   );
 }
 
