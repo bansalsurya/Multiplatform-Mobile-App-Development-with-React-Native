b65710a264b2239d7a2f1a6ab950f45cb34627f9
bansalsurya <2647surya@gmail.com>
2020-06-29 01:26:12 +0530

    git persiste store

--- react-native/confusion/App.js
+++ react-native/confusion/App.js
@@ -2,13 +2,17 @@ import React, { Component } from 'react';
 import Main from './components/MainComponent';
 import { Provider } from 'react-redux';
 import { ConfigureStore } from './redux/configureStore';
+import { PersistGate } from 'redux-persist/es/integration/react';
+import { Loading } from './components/LoadingComponent';
 
+const { persistor, store } = ConfigureStore();
 class App extends Component {
   render() {
-    const store = ConfigureStore();
     return (
       <Provider store={store}>
-        <Main />
+        <PersistGate loading={<Loading />} persistor={persistor}>
+          <Main />
+        </PersistGate>
       </Provider>
     );
   }
